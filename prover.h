121abcb57cdff5b727361a2ad9f171dd24210355
tyleregan <TylerEgan@u.boisestate.edu>
2017-12-02 12:42:39 -0700

    Now works with .c
    ifdefine won't work for someone reason. Will worry about it later.

--- prover.h
+++ prover.h
@@ -1,5 +1,9 @@
-#IFNDEF __PROVER_H
-#DEFINE __PROVER_H
+/*
+	prover.h: Defines the functions for prover.c
+*/
+
+//#ifndef __PROVER_H
+//#define __PROVER_H
 
 /*CONSTANTS*/
 
@@ -50,61 +54,83 @@ typedef struct {
 
 /*FUNCTION HEADERS*/
 
-int *OrderByPreds(int rSent, int rPreds[]);
-/* Returns true if the parameter is a constant */
-int constant(Parameter param);
-/* Returns true if the parameter is a variable */
-int variable(Parameter param);
-/* Returns true if the parameter is empty */
-int empty(Parameter *param);
-/* Set the KB to empty */
-void InitializeKB(void);
+/* Allow user to enter a sentence to be added to KB */
+void AddKBSentence(void);
+
 /* Add a predicate to the predicate list */
 int AddPredicate(char *name, int numparam);
+
+/* TODO Explanation Comment */
+void addPredicateWithSkip(int destSent, int srcSent, int skipPred);
+
+/* Add a sentence to the KB */
+void AddSentence(int neg[MAXPRED], int pred[MAXPRED],
+                 char param[MAXPRED][MAXPARAM][16],  int snum, char *leftover);
+
+/* TODO Explanation Comment */
+void AddSentenceFromResolution(int s1, int s2, int p1, int p2, Assignment *theta, int numAssign);
+
+/* Returns true if the parameter is a constant */
+int constant(Parameter param);
+
+/* Returns true if the parameter is empty */
+int empty(Parameter *param);
+
+/* Set the KB to empty */
+void InitializeKB(void);
+
+/* Load a KB from a text file */
+void LoadKB(void);
+
+/* Order sentences by number of preds in common that are also negated.*/
+int *OrderByPreds(int rSent, int rPreds[]);
+
+/* TODO Explanation Comment */
+void performSubstitutions(int s, Assignment *theta ,int numAssign);
+
+/* TODO Explanation Comment */
+void printAssignments(Assignment *theta, int numAssign);
+
+/* TODO Explanation Comment */
+void printParam(Parameter p);
+
+/* User enters a the negation of their query.  This is added to KB, and then KB is resolved to find solution */
+void ProveQuery(void);
+
+/* Read in a KB from a text file */
+int ReadKB(char *filename);
+
+/* TODO Explanation Comment */
+void replaceVar(Parameter *start, Parameter *end, int var, int val);
+
+/* You must write this function */
+void Resolve(void);
+
+/* You must write this function */
+void ResolveHeuristic();
+
+/* You must write this function */
+void ResolveRandom();
+
+/* Print the current KB to the screen */
+void ShowKB(void);
+
 /* Standardize apart (Makes sure that each sentence has unique variables) */
 void Standardize(char param[MAXPRED][MAXPARAM][16],
                  Parameter sparam[MAXPRED][MAXPARAM],
                  int pred[MAXPRED], int snum);
-/* Add a sentence to the KB */
-void AddSentence(int neg[MAXPRED],
-                 int pred[MAXPRED],
-                 char param[MAXPRED][MAXPARAM][16],
-                 int snum, char *leftover);
-/* Convert text version of a sentence into internal representation */
-int StringToSentence(char *line);
-/* Read in a KB from a text file */
-int ReadKB(char *filename);
-/* Load a KB from a text file */
-void LoadKB(void);
-/* Print the current KB to the screen */
-void ShowKB(void);
-/* Allow user to enter a sentence to be added to KB */
-void AddKBSentence(void);
-/* TODO Explanation Comment */
-int Unify(int sent1, int sent2, Assignment *Theta);
-/* You must write this function */
-void RandomResolve();
-/* Order sentences by number of preds in common that are also negated.*/
-int *OrderByPreds(int rSent, int rPreds[]);
-/* You must write this function */
-void HeuristicResolve();
-/* You must write this function */
-void Resolve(void);
-/* User enters a the negation of their query.  This is added to KB, and then KB is resolved to find solution */
-void ProveQuery(void);
-/* TODO Explanation Comment */
-void addPredicateWithSkip(int destSent, int srcSent, int skipPred);
-/* TODO Explanation Comment */
-void AddSentenceFromResolution(int s1, int s2, int p1, int p2, Assignment *theta);
-/* TODO Explanation Comment */
-void performSubstitutions(int s, Assignment *theta);
-/* TODO Explanation Comment */
-void printAssignments(Assignment *theta);
-/* TODO Explanation Comment */
-void printParam(Parameter p);
-/* TODO Explanation Comment */
-void replaceVar(Parameter *start, Parameter *end, int var, int val);
+
 /* TODO */
 void StandardizeApartVariables(int s);
+
+/* Convert text version of a sentence into internal representation */
+int StringToSentence(char *line);
+
 /* TODO */
 int tryResolution(int sent1, int sent2);
+
+/* TODO Explanation Comment */
+int UnifyPred(int sent1, int p1, int sent2, int p2, Assignment *Theta);
+
+/* Returns true if the parameter is a variable */
+int variable(Parameter param);
